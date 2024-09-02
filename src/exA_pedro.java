db517f0203e4a7bf833369172421d0fbe2086f10
xsalles <xs.salles@gmail.com>
2024-09-02 16:22:41 -0300

    feat: exercise A.

---
+++ src/exA_pedro.java
@@ -0,0 +1,24 @@
+package src;
+import java.util.Scanner;
+
+public class exA_pedro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, r;
+
+        System.out.println("Digite o valor de A");
+        a = sc.nextInt();
+
+        System.out.println("Digite o valor de B");
+        b = sc.nextInt();
+
+        if (a >= b){
+            r = a - b;
+        } else {
+            r = b - a;
+        }
+
+        System.out.println("O valor da diferença entre A e B é " + r);
+    }
+}

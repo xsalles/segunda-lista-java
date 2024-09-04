a3619adfe662f27f019ed8736359c28656afc821
xsalles <xs.salles@gmail.com>
2024-09-04 14:05:45 -0300

    feat: exercise E.

--- src/exE_pedro.java
+++ src/exE_pedro.java
@@ -1,7 +1,24 @@
 package src;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class exE_pedro {
     public static void main(String[] args) {
-        
-    }
-}
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor de A");
+        a = sc.nextInt();
+
+        System.out.println("Digite o valor de B");
+        b = sc.nextInt();
+
+        System.out.println("Digite o valor de C");
+        c = sc.nextInt();
+
+        int [] numeros = new int[]{a, b, c};
+
+        Arrays.sort(numeros);
+
+        System.out.println(Arrays.toString(numeros));
+}}
